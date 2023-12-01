49f16c4a31ed19f832e0e3456d4d80f8461bfd42
BilalSamiMirza <bilalsamimirza@gmail.com>
2023-12-02 00:07:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//alert("hellojavascripht")
+//alert("This page is responsive?")
+//var firstPersonName = "zaid"
+//console.log(firstPersonName)
+
+
+//var marks = 75;
+//alert(marks)
+
+//var marks = 85;
+//alert(marks)
+
+
+//var a = 10;
+//var b = a++;
+//var b = a+1;
+//console.log(b, "B value");
+//console.log(a, "A value");
+//var userName = prompt("Enter Your Name");
+//var age = prompt("Enter Your Age");
+//console.log(userName, age);
+var totalmarks=550;
+
+
+var a =parseInt(prompt ("Enter marks of physics"));
+var b =parseInt(prompt ("Enter marks of computer"));
+var c =parseInt(prompt ("Enter marks of english"));
+var d =parseInt(prompt ("Enter marks of mathematics"));
+var e =parseInt(prompt ("Enter marks of urdu"));
+var all = (a + b + c + d + e );
+alert(all);
+var percentage = (all*100)/totalmarks;
+alert(percentage);
